af4bafc3886dac67d989fbfbabfa1d8d81e28a40
Rokas <rookas.rudzenskas@gmail.com>
2021-05-11 15:49:13 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import React, {Component} from "react";
-import {StyleSheet, View, SafeAreaView, Text} from "react-native";
+import React, {Component, useState} from "react";
+import {StyleSheet, View, SafeAreaView, Text, TextInput, Button, TouchableOpacity} from "react-native";
 import Todo from "./Todo";
 
 // safe area stops going an app elements to the top nothch on iphone
 const App = () => {
+
+    const [input, setInput] = useState();
+    const [todos, setTodos] = useState();
+
+    const addTodo = () => {
+        setTodos([input, ...todos]);
+        setInput("");
+    };
+
   return (
       // safe area
       <SafeAreaView>
         {/* divs, in here it is views*/}
-        <View  >
-          <Todo title="Go To get bananas"/>
-          <Todo title="Go to sleep" />
-          <Todo title="Go to sleep"/>
-          <Todo title="Go to sleep"/>
-          <Todo title="Go to sleep"/>
-        </View>
+          <View>
+              <Text style={styles.titleText}>Lets build amazing app</Text>
+          </View>
 
+          {/*{todos.map(todo => (*/}
+          {/*    <Todo title={todo} />*/}
+          {/*))}*/}
+            <TextInput style={styles.app__input} value={input} onChangeText={text => setInput(text)} placeholder="Rookas"/>
+            <TouchableOpacity title="Add TODO" onPress={addTodo}/>
       </SafeAreaView>
   );
 }
@@ -24,6 +34,13 @@ const styles = StyleSheet.create({
     firstLabel: {
         fontSize: 30,
         fontWeight: "bold",
+    },
+    app__input: {
+        borderRadius: 999,
+        margin: 20,
+        height: 40,
+        borderColor: "red",
+        borderWidth: 1,
     },
 
 });
