6c15db8e9d4e041d74a0a93476b5cbbaa0d62e95
Rookas Rudzianskas <rookasrudzianskas@Rookass-MacBook-Pro-2.local>
2021-05-14 17:51:30 +0300

    Rokas

--- LoginPage.js
+++ LoginPage.js
@@ -1,16 +1,44 @@
-import React from "react";
-import {Button, Text, View} from "react-native";
+import React, {useState} from "react";
+import {Button, StyleSheet, Text, TextInput, View} from "react-native";
 
 
 
 
 const LoginPage = ({navigation}) => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <View>
             <Text>I am Login</Text>
+
+            <TextInput
+                style={styles.input}
+                value={username}
+                type="text"
+                placeholder="Enter a username"
+                onChangeText={text => setUsername(text)}
+            />
+
+            <TextInput
+                style={styles.input}
+                value={password}
+                secureTextEntry={true}
+                placeholder="Enter a password"
+                onChangeText={text => setPassword(text)}
+            />
             <Button title="Go back" onPress={() => navigation.goBack()}/>
         </View>
     )
 };
 export default LoginPage;
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 30,
+        margin: 20,
+        height: 40,
+        borderColor: "lightgray",
+        borderWidth: 1,
+    }
+});
